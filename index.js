38ba4d2fc7b83259092f65905265382c3e40e4a6
buyi <huaqirong@126.com>
2016-01-27 23:45:43 +0800

    js var

---
+++ index.js
@@ -0,0 +1,9 @@
+/* 
+ * @Author: buyi
+ * @Date:   2016-01-27 23:19:43
+ * @Last Modified by:   buyi
+ * @Last Modified time: 2016-01-27 23:21:10
+ */
+
+'use strict';
+alert('Hello11');
---
+++ study.js
@@ -0,0 +1,9 @@
+/* 
+* @Author: buyi
+* @Date:   2016-01-27 23:22:19
+* @Last Modified by:   buyi
+* @Last Modified time: 2016-01-27 23:22:56
+*/
+
+'use strict';
+var x = 1;
